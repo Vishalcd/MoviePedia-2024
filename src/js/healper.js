58f1613f1538c0e0a922b5fad26d28041639521f
Vishalcd <vishalbhatipersonal@gmail.com>
2024-02-14 21:59:40 +0530

    Add new Feature

--- src/js/healper.js
+++ src/js/healper.js
@@ -9,6 +9,7 @@ export const OPTIONS = {
 
 export function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
+  console.log('dfefe');
 }
 
 console.log('new feature');
